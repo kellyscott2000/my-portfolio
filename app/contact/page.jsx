05cd276cbf19672d61a5e6d7f7131dabeca01497
kellyscott2000 <92828904+kellyscott2000@users.noreply.github.com>
2025-05-18 16:48:12 +0100

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -96,10 +96,15 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form
+            {/* <form
               onSubmit={handleSubmit}
               className="flex flex-col gap-6 p-10 bg-[#27272c]  rounded-xl"
-            >
+            > */}
+
+              <form action="https://getform.io/f/akkngnza" 
+              className="flex flex-col gap-6 p-10 bg-[#27272c]  rounded-xl"
+              
+              method="POST">
               <h3 className="text-4xl text-amber-400">Let's work together</h3>
               <p className="text-white/60 text-justify">
                 I'm always excited to collaborate on new projects, whether
